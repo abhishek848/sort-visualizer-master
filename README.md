54b95a691f2d17702884a40317d88fc5ceca7dd3
abhishek848 <65775556+abhishek848@users.noreply.github.com>
2022-06-28 14:46:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Visualizer
+ We build this product to perform sorting with the given reandom data.
---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+    return ( <
+        footer className = "Footer" >
+        <
+        section >
+        Designed and built by Abhishek and Supreet
+
+        <
+        /section>     
+
+        <
+        /footer>
+    );
+};
+
+export default Footer;
